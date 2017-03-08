491c77f7eb15a11039fba42a5a1a1765e9f81d19
Drew Mace <dremac1@live.com>
2017-03-08 15:12:08 +0000

    spacing

--- models/address_book.rb
+++ models/address_book.rb
@@ -3,22 +3,27 @@ require_relative 'entry'
 class AddressBook
 	attr_reader :entries
 	
-	def initialize 
+	def initialize
 		@entries = []
 	end
-
 	
-   def add_entry(name, phone_number, email)
-     # #9
-     index = 0
-     entries.each do |entry|
-     # #10
-       if name < entry.name
-         break
-       end
-       index+= 1
-     end
-     # #11
-     entries.insert(index, Entry.new(name, phone_number, email))
-   end
- end
+	def add_entry(name, phone_number, email)
+		index = 0
+		entries.each do |entry|
+			if name < entry.name
+				break
+				index += 1
+			end
+			
+			entries.insert(index, Entry.new(name, phone_number, email))
+		 end
+		
+		def remove_entry(name, phone_number, email)
+			entries.each do |entry|
+				if name == entry.name
+					entries.delete(name, phone_number, email)
+				end
+			end
+		end
+  end
+end
