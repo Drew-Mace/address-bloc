9ae336684acce92871c9ce55eab772e19562c9a7
Drew Mace <dremac1@live.com>
2017-03-09 16:28:08 +0000

    csv-assignment

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -90,5 +90,32 @@ RSpec.describe AddressBook do
        entry_five = book.entries[4]
        check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
      end
-    end
-  end
+   end
+   
+   describe "#import_from_csv with entries_2.csv" do 
+     it "imports correct number of entries" do
+       book.import_from_csv("entries_2.csv")
+       book_size = book.entries.size
+       
+       expect(book_size).to eq 3
+     end
+     
+     it "imports the 1st entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_one = book.entries[0]
+       check_entry(entry_one, "Dave", "610-420-7641", "dhunsicker@live.com")
+     end
+     
+     it "imports the 2nd entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Drew", "123-456-7890", "dmace@live.com")
+     end
+     
+     it "imports the 2nd entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_three = book.entries[2]
+       check_entry(entry_three, "Jim", "484-343-5678", "jbernhardt@live.com")
+     end
+   end
+ end
